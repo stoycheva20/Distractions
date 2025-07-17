a61ca509e2171c5e228712a3c579fbd6a87f02cd
stoycheva20 <107383377+stoycheva20@users.noreply.github.com>
2025-07-17 00:11:08 -0400

    The new

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-const ideaText = document.getElementById('ideaText');
-const jarImage = document.getElementById('jarImage');
-const sound = document.getElementById('sparkleSound');
 
 const ideas = [
-  "Piano", "Violin", "Reading self-help", "Procreate drawing",
-  "Drawing on paper", "Twerking", "Belly dancing", "Reading for fun",
-  "Trim nails", "Gold hairdo", "Cool eyeliner", "Train backflip",
-  "Walk in nature (White Water Park)", "Judo", "Hapkido", "New language"
+  "Piano", "Violin", "Reading self help", "Procreate drawing", "Drawing on paper",
+  "Twerking", "Belly dancing", "Reading for fun", "Trim nails", "Gold hairdo",
+  "Cool eyeliner", "Train backflip", "Walk in nature (White Water Park)",
+  "Judo", "Hapkido", "New language"
 ];
 
-jarImage.addEventListener('click', () => {
+const jar = document.getElementById('jarImage');
+const text = document.getElementById('ideaText');
+
+jar.addEventListener('click', () => {
   const idea = ideas[Math.floor(Math.random() * ideas.length)];
-  ideaText.textContent = idea;
-  sound.currentTime = 0;
-  sound.play();
+  text.textContent = idea;
 });
